4adc6ad2825fad512d4e1b567a508e814e66c753
yassssss-369 <pes1202201465@pesu.pes.edu>
2024-10-02 03:50:03 +0530

    hi

---
+++ single_stage_processor/single_stage_procesor.srcs/sources_1/new/mux2.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02.10.2024 02:44:55
+// Design Name: 
+// Module Name: mux2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux2(
+
+    );
+endmodule
