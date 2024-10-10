e56adc7ee6b3d09488682fb68e1e68345d25df1c
ShriyaD33 <pes1202201539@pesu.pes.edu>
2024-10-10 22:51:55 +0530

    updated_with_master_tb_sim_file

---
+++ single_stage_processor/single_stage_procesor.srcs/sim_1/new/tb_master_riscv.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.10.2024 22:50:54
+// Design Name: 
+// Module Name: tb_master_riscv
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_master_riscv(
+
+    );
+endmodule
